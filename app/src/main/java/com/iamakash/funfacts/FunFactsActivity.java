16baca0598957ce8cdb16efb9df2352a346bfffd
AkashMalhotra <raz0rstr@hotmail.com>
2015-07-31 21:48:24 -0400

    shake detector improvements

--- app/src/main/java/com/iamakash/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/iamakash/funfacts/FunFactsActivity.java
@@ -21,40 +21,59 @@ public class FunFactsActivity extends Activity {
     private ShakeDetector mShakeDetector;
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
-    private factBook mFactBook=new factBook();
-    private colorwheel mcolorwheel= new colorwheel();
+
+    private factbook mFactBook = new factbook();
+    private colorwheel mcolorwheel = new colorwheel();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+
+        //declare view variables
+
         final TextView factlabel = (TextView) findViewById(R.id.facttextView);
         final Button showFactButton = (Button) findViewById(R.id.showfactbutton);
         final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        View.OnClickListener Listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact = mFactBook.getFact();
+                //update label with dynamic fact
+                factlabel.setText(fact);
+                int color = mcolorwheel.getcolor();
+                relativeLayout.setBackgroundColor(color);
+            }
+        };
 
-
-
+        showFactButton.setOnClickListener(Listener);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
-
             @Override
             public void onShake() {
-                mFactBook.getFact();
+                String fact = mFactBook.getFact();
+                //update label with dynamic fact
+                factlabel.setText(fact);
+                int color = mcolorwheel.getcolor();
+                relativeLayout.setBackgroundColor(color);
             }
-        }
-        );
+        });
+    }
 
+    @Override
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        // Registers (mShakeDetector which implements) SensorEventListener
     }
+
+    @Override
     protected void onPause() {
         mSensorManager.unregisterListener(mShakeDetector);
         super.onPause();
     }
-    //declare view variables
-
-        }
-        }
+}
